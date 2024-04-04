8f8722612f3dbd1b55e0cfef1d5c42944a459041
ernishar <nalam.netclues@gmail.com>
2024-04-04 10:01:32 +0530

    initial commit

--- frontend/src/Components/01-04/Constants.js
+++ frontend/src/Components/01-04/Constants.js
@@ -1,3 +1,3 @@
-export const profilePicURL = "http://localhost:4000";
+export const profilePicURL = "http://localhost:4000/public/profilePics";
 export const defaultProfilePic =
   "https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper.png";
